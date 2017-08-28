90129b8712d8b1c6d17f27217d198bddcec5873b
tetraeder <gad.salner@gmail.com>
2017-08-28 12:11:36 +0300

    assets

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,7 @@ module Myapp
 
     config.assets.enabled = true
     # config.assets.paths << Rails.root.join('/app/assets/fonts')
+    config.serve_static_assets = true
 
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
 
--- config/webpack/production.js
+++ config/webpack/production.js
@@ -29,7 +29,7 @@ module.exports = merge(sharedConfig, {
     new CompressionPlugin({
       asset: '[path].gz[query]',
       algorithm: 'gzip',
-      test: /\.(js|css|html|json|ico|svg|eot|otf|ttf)$/
+      test: /\.(js|css|html|json|ico|svg|eot|otf|ttf|jpg)$/
     })
   ]
 })
